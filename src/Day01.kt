696d260a3cc87b0287f0c7227bc2029c3105c3fd
Veniamin Viflyantsev <knok16@gmail.com>
2023-12-01 12:11:43 +0400

    day01 task

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = input.sumOf { line ->
+        (line.first { it.isDigit() } - '0') * 10 + (line.last { it.isDigit() } - '0')
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun part2(input: List<String>): Int = input.sumOf { line ->
+        val digits = line.mapIndexedNotNull { index, c ->
+            if (c.isDigit()) c - '0'
+            else (numbers.indexOfFirst { line.startsWith(it, index) } + 1).takeIf { it > 0 }
+        }
+
+        digits.first() * 10 + digits.last()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test_2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    println(part1(input))
+    println(part2(input))
 }
