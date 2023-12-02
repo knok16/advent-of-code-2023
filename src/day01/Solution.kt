bfa01d9f826810356949e35b162e1dcc2837188e
Veniamin Viflyantsev <knok16@gmail.com>
2023-12-02 15:25:32 +0400

    add package

--- src/day01/Solution.kt
+++ src/day01/Solution.kt
@@ -1,3 +1,7 @@
+package day01
+
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int = input.sumOf { line ->
         (line.first { it.isDigit() } - '0') * 10 + (line.last { it.isDigit() } - '0')
@@ -15,12 +19,12 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("day01/test")
     check(part1(testInput) == 142)
-    val testInput2 = readInput("Day01_test_2")
+    val testInput2 = readInput("day01/test_2")
     check(part2(testInput2) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/input")
 
     println(part1(input))
     println(part2(input))
